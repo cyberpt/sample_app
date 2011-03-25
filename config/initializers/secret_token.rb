d2ec1902cdf8513deba2f187613a14129f690bb6
cyberpt <cyberpt@gmail.com>
2011-03-25 20:50:19 +1100

    initial commeit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '31f7bbf2351843d4fc1dc575963d40354312b5419f28fa941811e927d9468045e768101b04508f9d57e2aa4542c114fac5cd950190bfc3056129540e196836b2'
